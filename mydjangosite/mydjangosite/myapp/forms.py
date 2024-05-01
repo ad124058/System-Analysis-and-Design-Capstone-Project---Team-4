cf1d30b86483123972a9816dcf02563bda68f9ef
AustinSteiniger <158093065+AustinSteiniger@users.noreply.github.com>
2024-05-01 02:46:22 -0500

    Update forms.py

--- mydjangosite/mydjangosite/myapp/forms.py
+++ mydjangosite/mydjangosite/myapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
-
+# this will create a class for the name form from the imports
 class NameForm(forms.Form):
     your_name = forms.CharField(label="Your name", max_length=100)
