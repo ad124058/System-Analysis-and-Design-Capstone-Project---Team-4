874effc864972255b5e3613abc8d414bfc904aeb
AustinSteiniger <158093065+AustinSteiniger@users.noreply.github.com>
2024-05-01 02:44:31 -0500

    Update apps.py

--- mydjangosite/mydjangosite/myapp/apps.py
+++ mydjangosite/mydjangosite/myapp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-'this will creat a new class for the app import'
+# this will creat a new class for the app import
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
